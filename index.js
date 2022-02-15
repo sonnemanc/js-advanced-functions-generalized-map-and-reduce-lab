9a63c7bfd338f57b0828dc1b5abfde43ac81dcca
Corban <sonnemanc@gmail.com>
2022-02-15 15:59:43 -0800

    commit tree c92cac403c4163e0f9dcfacaa5b89648e1309188

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, funk) {
+  let array = [];
+  sourceArray.forEach(element => {
+      array.push(funk(element))
+  });
+  return array
+}
+
+function reduce(sourceArray, funk, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < sourceArray.length; i++) {
+            total = funk(sourceArray[i], total);
+        }
+        return total;
+    } else {
+        total = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = funk(sourceArray[i], total);
+        }
+        return total;
+    }
+}
